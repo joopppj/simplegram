265c86b1751ad988f3dee440572eb7b691e169f1
kira <zhousenyeliu@gmail.com>
2021-03-04 21:35:10 +0900

    Initial Commit

---
+++ simplegram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  simplegram
+//
+//  Created by りゅひかる on 2021/03/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ simplegram/simplegramApp.swift
@@ -0,0 +1,17 @@
+//
+//  simplegramApp.swift
+//  simplegram
+//
+//  Created by りゅひかる on 2021/03/04.
+//
+
+import SwiftUI
+
+@main
+struct simplegramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
